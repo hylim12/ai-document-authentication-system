8775e32d3a2ef47d2b8619dd9987c2b4d186cfe7
hylim12 <139207344+hylim12@users.noreply.github.com>
2026-03-24 12:03:40 +0800

    Ensure _is_valid_for_field exists in class with strict field rules

---
+++ prompts/regex_passport_patterns.py
@@ -0,0 +1,35 @@
+LABEL_PATTERNS = {
+    "SURNAME": [
+        r"SURNAME", r"MBIEMR", r"UZVĀRDS", r"PRIEZVISKO"
+    ],
+    "GIVEN NAME": [
+        r"GIVEN", r"EMR", r"VĀRDS", r"MENO"
+    ],
+    "NATIONALITY": [
+        r"NATIONALITY", r"SHTET", r"PILSON", r"ŠTÁT"
+    ],
+    "DATE OF BIRTH": [
+        r"BIRTH", r"LINDJ", r"DZIM", r"NAROD"
+    ],
+    "DATE OF ISSUE": [
+        r"ISSUE", r"LSHIM", r"IZDOŠ", r"VYDANIA"
+    ],
+    "DATE OF EXPIRY": [
+        r"EXPIR", r"SKADIM", r"DERĪGUMA", r"PLATNOSTI"
+    ],
+    "SEX": [
+        r"SEX", r"GJIN", r"DZIMUMS", r"POHLAVIE"
+    ],
+    "ID CARD NO": [
+        r"ID", r"CARD", r"NR", r"ČÍSLO"
+    ],
+    "PERSONAL NO": [
+        r"PERSONAL", r"KODS", r"ČÍSLO"
+    ],
+    "AUTHORITY": [
+        r"AUTHORITY", r"AUTOR", r"IESTĀDE"
+    ],
+    "HEIGHT": [
+        r"HEIGHT", r"TAILLE", r"AUGUMS", r"CM"
+    ]
+}
