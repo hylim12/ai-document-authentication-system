14ff0a9cae985f0fd2ba8f9a3b1d48a97cfcdd8a
hylim12 <139207344+hylim12@users.noreply.github.com>
2026-03-25 15:05:51 +0800

    25thMar_v5

--- prompts/regex_passport_patterns.py
+++ prompts/regex_passport_patterns.py
@@ -1,41 +1,81 @@
 LABEL_PATTERNS = {
+
+    # --- NAME FIELDS ---
     "SURNAME": [
-        r"SURNAME", r"MBIEMR", r"UZVĀRDS", r"PRIEZVISKO"
+        r"SURNAME", r"MBIEMRI", r"UZV[ĀA]RDS", r"PRIEZVISKO", r"UZVARDS"
     ],
     "GIVEN NAME": [
-        r"GIVEN", r"EMR", r"VĀRDS", r"MENO"
+        r"GIVEN\s*NAME", r"EMRI", r"V[ĀA]RDS", r"MENO", r"GIVEN\s*NAMES?"
     ],
+
+    # --- NATIONALITY ---
     "NATIONALITY": [
-        r"NATIONALITY", r"SHTET", r"PILSON", r"ŠTÁT"
+        r"NATIONALITY", r"SHTETESIA", r"PILSONIBA", r"ŠT[ÁA]TNE\s*OBČIANSTVO", r"OBČIANSTVO", r"VALSTS"
     ],
+
+    # --- DATES ---
     "DATE OF BIRTH": [
-        r"BIRTH", r"LINDJ", r"DZIM", r"NAROD"
+        r"DATE\s*OF\s*BIRTH", r"BIRTH", r"LINDJA", r"DZIM", r"NAROD", r"DZIMŠANAS\s*DATUMS"
     ],
     "DATE OF ISSUE": [
-        r"ISSUE", r"LSHIM", r"IZDOŠ", r"VYDANIA"
+        r"DATE\s*OF\s*ISSUE", r"ISSUE", r"LESHIMIT", r"IZDOŠ", r"VYDANIA", r"IZSniegšanas"
     ],
     "DATE OF EXPIRY": [
-        r"EXPIR", r"SKADIM", r"DERĪGUMA", r"PLATNOSTI"
+        r"DATE\s*OF\s*EXPIRY", r"EXPIRY", r"SKADIMIT", r"DER[ĪI]GUMA", r"PLATNOSTI", r"DERĪGS\s*LĪDZ"
     ],
+
+    # --- SEX ---
     "SEX": [
-        r"SEX", r"GJIN", r"DZIMUMS", r"POHLAVIE"
+        r"SEX", r"GJINIA?", r"DZIMUMS", r"POHLAVIE"
     ],
+
+    # --- DOCUMENT NUMBERS ---
     "ID CARD NO": [
-        r"ID", r"CARD", r"NR", r"ČÍSLO"
+        r"ID\s*CARD\s*NO", r"CARD\s*NO", r"NR\.\s*LET", r"Č[ÍI]SLO", r"\bNO\.\b"
     ],
     "PASSPORT NO": [
-        r"PASSPORT", r"PASE", r"NR", r"NUM"
+        r"PASSPORT\s*NO", r"PASE", r"PASSPORT", r"NR", r"NUM", r"PAS\.?\s*NR"
     ],
     "PERSONAL NO": [
-        r"PERSONAL", r"KODS", r"ČÍSLO"
+        r"PERSONAL\s*NO", r"KODS", r"PERSONAS\s*KODS", r"RODN[ÉE]\s*Č[ÍI]SLO"
     ],
+
+    # --- LOCATION ---
     "PLACE OF BIRTH": [
-        r"PLACE OF BIRTH", r"BIRTHPLACE", r"VENDLIND", r"DZIMSANAS VIETA", r"MIESTO NARODENIA"
+        r"PLACE\s*OF\s*BIRTH", r"BIRTHPLACE", r"VENDLINDJA",
+        r"DZIMŠANAS\s*VIETA", r"MIESTO\s*NARODENIA", r"LIEU\s*DE\s*NAISSANCE"
     ],
+
+    # --- AUTHORITY / ISSUER ---
     "AUTHORITY": [
-        r"AUTHORITY", r"AUTOR", r"IESTĀDE"
+        r"AUTHORITY", r"AUTORITETI", r"AUTOR", r"IEST[ĀA]DE",
+        r"IZDEV[ĒE]JS", r"ISSUED\s*BY", r"VYD[ÁA]L", r"IZSNIEDZA"
     ],
+
+    # --- HEIGHT ---
     "HEIGHT": [
-        r"HEIGHT", r"TAILLE", r"AUGUMS", r"CM"
+        r"HEIGHT", r"TAILLE", r"AUGUMS", r"\bCM\b"
+    ],
+
+    # Passport Type (Latvia)
+    "TYPE": [
+        r"\bTYPE\b", r"TIPS?", r"TIPS"
+    ],
+
+    # Issuing State Code (Latvia)
+    "ISSUING STATE CODE": [
+        r"CODE\s*OF\s*ISSUING\s*STATE",
+        r"VALSTS\s*KODS",
+        r"CODE\s*DU\s*PAYS"
+    ],
+
+    # Issued By (Slovakia specific)
+    "ISSUED BY": [
+        r"ISSUED\s*BY", r"VYDAL", r"IZDEVIS", r"ISSUER"
+    ],
+
+    # Signature presence (no OCR value, but label detection)
+    "SIGNATURE": [
+        r"SIGNATURE", r"FIRMA", r"PARAKSTS", r"PODPIS", r"HOLDER'?S\s*SIGNATURE"
     ]
 }
